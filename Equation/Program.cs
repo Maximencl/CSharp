5bdc2c3edcf94b90a20b706765fd72d71dfb5e8e
Maxime Nicolas <nico.maxime84@gmail.com>
2024-02-01 15:56:04 +0100

    Ajout de la base Equation

---
+++ Equation/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Entrez la valeur de a :");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Entrez la valeur de b :");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("L'équation a une infinité de solutions.");
+            }
+            else
+            {
+                Console.WriteLine("L'équation n'a pas de solution.");
+            }
+        }
+        else
+        {
+            double solution = -b / a;
+            Console.WriteLine("La solution de l'équation est x = " + solution);
+        }
